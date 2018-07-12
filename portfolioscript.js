fb1fe20219532b1c939c670f4966d0946c8cf107
Ekscursionist <larakek@gmail.com>
2018-07-12 15:14:58 -0400

    adding in some js

---
+++ portfolioscript.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    console.log("script has begun");
+
+    var wordguess = {
+        assigpic: 'assets/images/portpic/trainportpic.jpg';
+        assigname: "abc";
+        assigdescrip: "abcd";
+        assiglink: "abcde";
+    };
+
+    var pokemonrpg
+
+    var tolkientrivia
+
+    var cloudyapi
+
+    var trainscheduler
+
+});
