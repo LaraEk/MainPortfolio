c49d175c8ea923c276d424551062ae8c8b43f466
Ekscursionist <larakek@gmail.com>
2018-07-13 10:23:58 -0400

    a lil more JSON, a few more backgrounds, and prettying up the contact page

--- portfolioscript.js
+++ portfolioscript.js
@@ -2,38 +2,43 @@ $(document).ready(function() {
     console.log("script has begun");
 
     var wordguess = {
-        assigpic: 'assets/images/portpic/trainportpic.jpg',
-        assigname: "abc",
+        assigpic: 'assets/images/portpics/wordguessportpic.png',
+        assigname: "Tolkien Word Guess Game",
         assigdescrip: "abcd",
-        assiglink: "abcde",
+        assigimproves: "improvements I intend to make",
+        assiglink: "https://laraek.github.io/Hangman-Game/",
     };
 
     var pokemonrpg = {
-        assigpic: 'assets/images/portpic/trainportpic.jpg',
-        assigname: "abc",
+        assigpic: 'assets/images/portpics/eevee.png',
+        assigname: "Pokemon RPG Game",
         assigdescrip: "abcd",
-        assiglink: "abcde",
+        assigimproves: "improvements I intend to make",
+        assiglink: "https://laraek.github.io/Assignment-Four/",
     };
 
     var tolkientrivia = {
-        assigpic: 'assets/images/portpic/trainportpic.jpg',
+        assigpic: 'assets/images/portpics/tolkienportpic.jpg',
         assigname: "abc",
         assigdescrip: "abcd",
-        assiglink: "abcde",
+        assigimproves: "improvements I intend to make",
+        assiglink: "https://laraek.github.io/TriviaGame/",
     };
 
     var cloudyapi = {
-        assigpic: 'assets/images/portpic/trainportpic.jpg',
+        assigpic: 'assets/images/portpics/cloudsportpic.jpg',
         assigname: "abc",
         assigdescrip: "abcd",
-        assiglink: "abcde",
+        assigimproves: "improvements I intend to make",
+        assiglink: "https://laraek.github.io/Clouds-API",
     };
 
     var trainscheduler = {
         assigpic: 'assets/images/portpic/trainportpic.jpg',
         assigname: "abc",
         assigdescrip: "abcd",
-        assiglink: "abcde",
+        assigimproves: "improvements I intend to make",
+        assiglink: "https://laraek.github.io/Week-7",
     };
 
 });
