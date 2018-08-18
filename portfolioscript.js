682f2ca56c2469f7d7d8492465a85c9f5648d88c
Ekscursionist <larakek@gmail.com>
2018-08-18 14:24:50 -0400

    adding Handlebars HW

--- portfolioscript.js
+++ portfolioscript.js
@@ -1,44 +1,52 @@
 $(document).ready(function() {
     console.log("script has begun");
 
-    var wordguess = {
+var portfolioitems = [
+    {
+        name: wordguess,
         assigpic: 'assets/images/portpics/wordguessportpic.png',
         assigname: "Tolkien Word Guess Game",
         assigdescrip: "abcd",
         assigimproves: "improvements I intend to make",
         assiglink: "https://laraek.github.io/Hangman-Game/",
-    };
-
-    var pokemonrpg = {
+    }, {
+        name: pokemonrpg,
         assigpic: 'assets/images/portpics/eevee.png',
         assigname: "Pokemon RPG Game",
         assigdescrip: "abcd",
         assigimproves: "improvements I intend to make",
         assiglink: "https://laraek.github.io/Assignment-Four/",
-    };
-
-    var tolkientrivia = {
+    }, {
+        name: tolkientrivia,
         assigpic: 'assets/images/portpics/tolkienportpic.jpg',
         assigname: "abc",
         assigdescrip: "abcd",
         assigimproves: "improvements I intend to make",
         assiglink: "https://laraek.github.io/TriviaGame/",
-    };
-
-    var cloudyapi = {
+    }, {
+        name: cloudyapi,
         assigpic: 'assets/images/portpics/cloudsportpic.jpg',
         assigname: "abc",
         assigdescrip: "abcd",
         assigimproves: "improvements I intend to make",
         assiglink: "https://laraek.github.io/Clouds-API",
-    };
-
-    var trainscheduler = {
+    }, {
+        name: trainscheduler,
         assigpic: 'assets/images/portpic/trainportpic.jpg',
         assigname: "abc",
         assigdescrip: "abcd",
         assigimproves: "improvements I intend to make",
         assiglink: "https://laraek.github.io/Week-7",
-    };
+    }
+];
 
 });
+
+$("#portfoliothings").on("click", ".portpic", function() {
+    $("#portpicholder").attr("src", portfolioitems.assigpic);
+    $("#portnameholder").text(portfolioitems.assigname);
+    $("#portdescripholder").text(portfolioitems.assigdescrip);
+    // $("#portnameholder").text(portfolioitems.assigname);
+    // $("#portnameholder").text(portfolioitems.assigname);
+
+  };
